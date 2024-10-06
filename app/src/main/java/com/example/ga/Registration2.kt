bfdb0946b81ad45a3d0f4dd867bb2f47136118b4
Raisa Matrosova <matrosova-05@list.ru>
2024-10-06 20:47:21 +0630

    Commit Message

--- app/src/main/java/com/example/ga/Registration2.kt
+++ app/src/main/java/com/example/ga/Registration2.kt
@@ -1,6 +1,8 @@
 package com.example.ga
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,12 +11,15 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,7 +31,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.ga.components.CButton
@@ -35,16 +39,15 @@ import com.example.ga.components.DontHaveAccountRow
 import com.example.ga.ui.theme.fontFamily
 
 @Composable
-fun Registration2(
-    navController: NavHostController
-) {
-    Surface(
-        modifier = Modifier.fillMaxSize()
-    ) {
+fun Registration2(navController: NavHostController) {
+    var name by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
 
-
-        Box(modifier =  Modifier.fillMaxSize()){
-            Image(painter = painterResource(id = R.drawable.rfon),
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Image(
+                painter = painterResource(id = R.drawable.rfon),
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
                 modifier = Modifier.fillMaxSize()
@@ -53,59 +56,52 @@ fun Registration2(
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Top,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 40.dp)
+                modifier = Modifier.fillMaxSize().padding(horizontal = 40.dp)
             ) {
 
-                Image(painter = painterResource(id = R.drawable.logowhite),
+                Image(
+                    painter = painterResource(id = R.drawable.logowhite),
                     contentDescription = null,
-                    modifier = Modifier
-                        .padding(top = 30.dp)
-                        .height(200.dp)
-                        .width(200.dp)
-                        .padding(bottom = 30.dp)
+                    modifier = Modifier.padding(top = 30.dp).height(200.dp).width(200.dp).padding(bottom = 30.dp)
                 )
 
-                Text(text = "Регистрация",
+                Text(
+                    text = "Регистрация",
                     style = TextStyle(
                         fontSize = 28.sp,
                         fontFamily = fontFamily,
                         fontWeight = FontWeight(800),
                         color = Color.White,
                         textAlign = TextAlign.Center
-
                     ),
-                    modifier = Modifier
-                        .padding(bottom = 30.dp)
+                    modifier = Modifier.padding(bottom = 30.dp)
                 )
 
-                Text("Зарегистрируйтесь, чтобы получить каталог растений!",
+                Text(
+                    "Зарегистрируйтесь, чтобы получить каталог растений!",
                     style = TextStyle(
                         fontSize = 15.sp,
                         fontFamily = fontFamily,
                         textAlign = TextAlign.Center,
                         color = Color(0xFFF2F5C0)
                     ),
-                    modifier = Modifier
-                        .padding(bottom = 30.dp)
+                    modifier = Modifier.padding(bottom = 30.dp)
                 )
-
-
-                CTextField(hint = "Имя", value = "" )
-
-                CTextField(hint = "Email", value = "" )
-
-                CTextField(hint = "Пароль", value = "" )
+                CTextField(hint = "Имя", value = name, onValueChange = { name = it })
+                CTextField(hint = "Email", value = email, onValueChange = { email = it })
+                CTextField(hint = "Пароль", value = password, onValueChange = { password = it })
 
                 Spacer(modifier = Modifier.height(50.dp))
 
-                CButton(text = "Зарегистрироваться")
+                CButton(text = "Зарегистрироваться",
+                onClick = {
+                    navController.navigate("map")
+                }
+                )
 
-                Row(
-                    modifier = Modifier.padding(top=12.dp, bottom = 52.dp)
-                ){
-                    Text("Уже есть аккаунт?",
+                Row(modifier = Modifier.padding(top=12.dp, bottom=52.dp)) {
+                    Text(
+                        "Уже есть аккаунт?",
                         style = TextStyle(
                             fontSize = 18.sp,
                             fontFamily = fontFamily,
@@ -113,7 +109,8 @@ fun Registration2(
                         )
                     )
 
-                    Text(" Войти",
+                    Text(
+                        " Войти",
                         style = TextStyle(
                             fontSize = 18.sp,
                             fontFamily = fontFamily,
@@ -124,14 +121,9 @@ fun Registration2(
                             navController.navigate("login")
                         }
                     )
-
-
                 }
-
             }
-
         }
-
     }
 }
 
